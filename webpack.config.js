47375041d3e3c3dff14883c2f683f30ef1553cb8
taoqf <tao_qiufeng@126.com>
2019-05-23 13:20:41 +0800

    fix issues: https://github.com/kensnyder/quill-image-resize-module/pull/53 https://github.com/kensnyder/quill-image-resize-module/pull/59 https://github.com/kensnyder/quill-image-resize-module/pull/66

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./src/ImageResize.js",
-    output: {
-        path: path.join(__dirname, 'dist'),
-        library: 'ImageResize',
-        libraryTarget: 'umd',
-        filename: "image-resize.min.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.join(__dirname, 'src'),
-                exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": [["env", { "modules": false }]],
-                        "plugins": ["babel-plugin-transform-class-properties"]
-                    }
-                }]
-            },
-            {
-                test: /\.svg$/,
-                use: [{
-                    loader: 'raw-loader'
-                }]
-            }
-        ]
-    }
+	entry: "./src/ImageResize.js",
+	output: {
+		path: __dirname,
+		library: 'ImageResize',
+		libraryTarget: 'umd',
+		filename: "image-resize.min.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.join(__dirname, 'src'),
+				exclude: /(node_modules|bower_components)/,
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						"presets": [["env", { "modules": false }]],
+						"plugins": ["babel-plugin-transform-class-properties"]
+					}
+				}]
+			},
+			{
+				test: /\.svg$/,
+				use: [{
+					loader: 'raw-loader'
+				}]
+			}
+		]
+	}
 };
